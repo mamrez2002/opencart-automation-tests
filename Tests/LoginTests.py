fe2fac6abf38e6f9be1c8631c9f68b165a40de25
mamrez <mamrez.2002.s@gmail.com>
2025-05-04 21:59:20 +0330

    login page tests

---
+++ Tests/LoginTests.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+import undetected_chromedriver as uc
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
+import os, sys, time, unittest
+from random import randint as rnd
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from Pages.Login import LoginPage
+
+
+class RegisterTests(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = uc.Chrome(driver_executable_path=ChromeDriverManager().install())
+        cls.driver.implicitly_wait(10)
+        cls.register_page = LoginPage(cls.driver)
+
+
+    def setUp(self):
+        self.driver.get("https://demo.opencart.com/en-gb?route=account/register")
+        time.sleep(1)
+
+
+
+
